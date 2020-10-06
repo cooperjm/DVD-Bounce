59d4298d7254b0453a7fb4ba5dae700ea2924110
James Cooper <cooper.jm@outlook.com>
2020-10-05 20:14:36 -0400

    Change image start position, did some organization

--- sketch.js
+++ sketch.js
@@ -1,34 +1,53 @@
-let x = 200;
-let y = 200;
-let speed = 2;
-let speedY = 2;
-let img;
+let settings = {
+  x: null,
+  y: null,
+  w: null,
+  h: null,
+  speed_X: 2,
+  speed_Y: 2,
+  img: null
+}
+
 
 function preload() {
-  img = loadImage("dvdlogo-04.svg");
+  size();
+  settings.img = loadImage("dvdlogo-04.svg");
+  center();
+
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(settings.w, settings.h);
   imageMode(CENTER);
-
 }
 
 function draw() {
+  size();
+  createCanvas(settings.w, settings.h);
   background(0);
   stroke(255);
   strokeWeight(4);
   noFill();
-  image(img, x, y, 150, 75);
+  image(settings.img, settings.x, settings.y, 150, 75);
 
-  if (x > width - 75 || x < 75) {
-    speed *= -1;
+  if (settings.x > width - 75 || settings.x < 75) {
+    settings.speed_X *= -1;
   }
 
-  if (y > height - 40 || y < 40) {
-    speedY *= -1;
+  if (settings.y > height - 40 || settings.y < 40) {
+    settings.speed_Y *= -1;
   }
 
-  x += speed;
-  y += speedY;
+  settings.x += settings.speed_X;
+  settings.y += settings.speed_Y;
+}
+
+function center() {
+  settings.x = (windowWidth / 2);
+  settings.y = (windowHeight / 2);
+}
+
+function size() {
+  settings.w = windowWidth;
+  settings.h = windowHeight;
 }
