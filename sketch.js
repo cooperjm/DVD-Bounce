549d67c1e05b306039476226ab051a5192406b6e
cooperjm <cooper.jm@outlook.com>
2020-10-07 18:10:26 -0400

    Removed unnecessary code

--- sketch.js
+++ sketch.js
@@ -1,39 +1,20 @@
 let dvd;
 let dvd2;
 
-// To hold live values of browser window dimensions
-let portal = {
-  w: null,
-  h: null,
-}
-
-// Load up the initial window size
-function preload() {
-  size();
-}
-
 // Setting up the canvas with the images and window dimensions
 function setup() {
   imageMode(CENTER);
-  createCanvas(portal.w, portal.h);
+  createCanvas(windowWidth, windowHeight);
   dvd = new DvdLogo(100, 100);
   dvd2 = new DvdLogo(windowWidth - 100, windowHeight - 100);
 }
 
 // Logic for the logo/logos and canvas dimensions
 function draw() {
-  size();
-  createCanvas(portal.w, portal.h);
+  createCanvas(windowWidth, windowHeight);
   background(0);
   dvd.show();
   dvd.move();
   dvd2.show();
   dvd2.move();
-}
-
-// Fetches dimensons of browser window into portal object
-// This is to live adjust the canvas size
-function size() {
-  portal.w = windowWidth;
-  portal.h = windowHeight;
 }
