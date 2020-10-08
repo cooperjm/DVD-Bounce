48a98c7dcc0519cfb28f26375dfdba6ade17f813
cooperjm <cooper.jm@outlook.com>
2020-10-08 11:59:07 -0400

    Added bounce sound

--- dvdLogoClass.js
+++ dvdLogoClass.js
@@ -1,6 +1,6 @@
 // Logo class, takes x and y as arguments
 class DvdLogo {
-  constructor(posX, posY) {
+  constructor(posX, posY, _sound) {
     this.x = posX;
     this.y = posY;
     this.initSpeed_X = 2;
@@ -10,21 +10,26 @@ class DvdLogo {
     this.img = loadImage("dvdlogo-04.svg");
     this.imgWidth = 150;
     this.imgHeight = 75;
+    this.sound = _sound;
   }
 
   move() {
     // Sets the bounce boundary for X-axis
     if (this.x > width - this.imgWidth / 2) {
       this.speed_X = -this.initSpeed_X;
+      this.playBoop();
     } else if (this.x < this.imgWidth / 2) {
       this.speed_X = this.initSpeed_X;
+      this.playBoop();
     }
 
     // Sets the bounce boundary for Y-axis
     if (this.y + this.imgHeight / 2 > height) {
       this.speed_Y = -this.initSpeed_Y;
+      this.playBoop();
     } else if (this.y <= this.imgHeight / 2) {
       this.speed_Y = this.initSpeed_Y;
+      this.playBoop();
     }
 
     // Changes x and y coordinates changing the logos position on the screen
@@ -35,5 +40,9 @@ class DvdLogo {
   // Puts the image on the canvas
   show() {
     image(this.img, this.x, this.y, this.imgWidth, this.imgHeight);
+  }
+
+  playBoop() {
+    this.sound.play();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,13 +1,25 @@
 let logos = [];
 let numberOfLogos = 1;
+let boop;
+let slider;
+let txt = "Volume Off/On";
+
+function preload() {
+  soundFormats("mp3", "ogg");
+  boop = loadSound("boop.mp3");
+  boop.setVolume(0.05);
+}
 
 // Setting up the canvas with the images and window dimensions
 function setup() {
   imageMode(CENTER);
   createCanvas(windowWidth, windowHeight);
+  slider = createSlider(0, 0.2, 0, 0.2);
+  slider.position(10, 10);
+  slider.style("width", "30px");
 
   for (let i = 0; i < numberOfLogos; i++) {
-    logos[i] = new DvdLogo(random(windowWidth), random(windowHeight));
+    logos[i] = new DvdLogo(random(windowWidth), random(windowHeight), boop);
   }
 }
 
@@ -15,6 +27,10 @@ function setup() {
 function draw() {
   createCanvas(windowWidth, windowHeight);
   background(0);
+  boop.setVolume(slider.value());
+  textSize(18);
+  text(txt, 50, 13, 150, 50);
+  fill(255);
 
   for (let i = 0; i < logos.length; i++) {
     logos[i].show();
